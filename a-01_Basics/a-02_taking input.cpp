86094dd26bd7368dc1c2b5485d4da597831f53e1
souvik-mndal <souvkmndal@gmail.com>
2024-09-18 22:56:52 +0530

    taking input

---
+++ a-01_Basics/a-02_taking input.cpp
@@ -0,0 +1,9 @@
+//taking input and print it 
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    int a ;
+    cout << "Enter a numeric value : ";
+    cin>> a ;
+    cout<<"The input you gave is : "<<a ;
+}
