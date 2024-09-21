baf58e33d903487a5b9614b3776c54a8c6087e41
souvik-mndal <souvkmndal@gmail.com>
2024-09-21 23:31:03 +0530

    hollow rectangle pattern

---
+++ a-01_Basics/a-13_half pyramid.cpp
@@ -0,0 +1,19 @@
+/*WAP to print half pyramid pattern
+*
+* * 
+* * * 
+* * * *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cout <<"Enter the number of rows : " ;
+    cin >>n ;
+    for( int i=0 ; i<n ; i++){
+        for( int j=0 ; j<=i ; j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
