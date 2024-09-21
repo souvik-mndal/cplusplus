061c128804aad7774d1d5fd89ef6ecfbb9762ef8
souvik-mndal <souvkmndal@gmail.com>
2024-09-21 23:36:50 +0530

    numeric half pyramid pattern

---
+++ a-01_Basics/a-15_numeric half pyramid.cpp
@@ -0,0 +1,19 @@
+/*WAP to print numeric half pyramid pattern
+*
+* * 
+* * * 
+* * * *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cout <<"Enter the number of rows : " ;
+    cin >>n ;
+    for( int i=0 ; i<n ; i++){
+        for( int j=0 ; j<=i ; j++){
+            cout <<j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
