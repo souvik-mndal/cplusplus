508ef90017ac32dfbf2f39ef333ce16b8455b27d
souvik-mndal <souvkmndal@gmail.com>
2024-09-21 23:32:09 +0530

    half pyramid pattern

--- a-01_Basics/a-13_half pyramid.cpp
+++ a-01_Basics/a-13_half pyramid.cpp
@@ -12,7 +12,7 @@ int main(){
     cin >>n ;
     for( int i=0 ; i<n ; i++){
         for( int j=0 ; j<=i ; j++){
-            cout<<"*"<<" ";
+            cout <<"*"<<" ";
         }
         cout<<endl;
     }
