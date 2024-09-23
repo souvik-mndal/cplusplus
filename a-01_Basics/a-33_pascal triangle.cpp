fde021f385d23bf8c93ce18e9eae1f3166922517
souvik-mndal <souvkmndal@gmail.com>
2024-09-23 23:43:12 +0530

    pascal triangle  pattern

---
+++ a-01_Basics/a-33_pascal triangle.cpp
@@ -0,0 +1,21 @@
+/*WAP to print pascal triangle pattern
+1
+1 1
+1 2 1
+1 3 3 1
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ;
+    cout <<"Enter the number of rows : " ;
+    cin >>n ;
+    for( int i=1 ; i<=n ; i++){
+        int c = 1;
+        for( int j=1 ; j<=i ; j++){
+            cout <<c<<" ";
+            c = c*(i-j)/j;
+        }
+        cout<<endl;
+    }
+}
