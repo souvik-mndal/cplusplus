d11a6dc8adb95bc351386ae7713855ef045afdaa
souvik mndal <souvkmndal@gmail.com>
2024-10-18 23:10:00 +0530

    to take input and print it

---
+++ 01.Basics/02.take input.cpp
@@ -0,0 +1,9 @@
+//WAP to take input an print it
+#include<iostream>
+using namespace std;
+int main(){
+    int input;
+    cout<<"enter the number : ";
+    cin>>input;
+    cout<<"the input value is : "<<input;
+}
