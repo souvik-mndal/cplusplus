fb0a50675afbe163043df5b341dca08f22a8f83d
souvik mndal <souvkmndal@gmail.com>
2024-10-20 01:58:12 +0530

    print the square pattern

---
+++ 01.Basics/10.square pattern.cpp
@@ -0,0 +1,14 @@
+//WAP to print the square pattern
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of rows : ";
+    cin>>n;
+    for( int i=0 ; i<n ; i++){
+        for( int j=0 ; j<n ; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
