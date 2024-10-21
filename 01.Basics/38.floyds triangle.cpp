c59509dc78d8cec656f7b28ace8b330e0be9a950
souvik mndal <souvkmndal@gmail.com>
2024-10-21 22:54:52 +0530

    to print the floyd's triangle pattern

--- 01.Basics/38.floyds triangle.cpp
+++ 01.Basics/38.floyds triangle.cpp
@@ -12,7 +12,7 @@ int main(){
     int ele=1;
     for( int i=0 ; i<n ; i++ ){
         for( int j=0 ; j<=i ; j++ ){
-            cout<<ele++<<" ";
+            cout<<ele++<<" " ;
         }
         cout<<endl;
     }
