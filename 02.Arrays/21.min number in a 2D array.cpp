0310f8cb8c4a5b0f38d19dc028f2c7fe0e8b04d5
souvik mndal <souvkmndal@gmail.com>
2024-10-23 20:37:01 +0530

    to find the min number in a 2D array

---
+++ 02.Arrays/21.min number in a 2D array.cpp
@@ -0,0 +1,24 @@
+//WAP to find the min number in a 2D array
+#include<bits/stdc++.h>
+using namespace std;
+int mini( int arr[][3] , int n ){
+    int ans = arr[0][0];
+    for( int i=0 ; i<n ; i++ ){
+        for( int j=0 ; j<n ; j++ ){
+            if( ans > arr[i][j]){
+                ans = arr[i][j];
+            }
+        }
+    }
+    return ans;
+}
+int main(){
+    int arr[3][3];
+    for( int i=0 ; i<3 ; i++ ){
+        for( int j=0 ; j<3 ; j++ ){
+            cout<<"enter the value for row "<<i<<" and column "<<j<<" : ";
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"The min element is : "<<mini( arr , 3 );
+}
