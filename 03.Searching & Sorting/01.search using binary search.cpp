c97ccf12d5fa213cac0cade3c622f5cdfcac5f8b
souvik mndal <souvkmndal@gmail.com>
2024-11-02 20:21:27 +0530

    search element using binary search

--- 03.Searching & Sorting/01.search using binary search.cpp
+++ 03.Searching & Sorting/01.search using binary search.cpp
@@ -4,8 +4,18 @@ using namespace std;
 int BS( int arr[] , int n , int x ){
     int s=0 , e=n-1;
     while( s<=e ){
-        
+        int mid = s=(e-s)/2;
+        if( arr[mid]==x ){
+            return mid;
+        }
+        else if( arr[mid]<x ){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
     }
+    return -1;
 }
 int main(){
 
