d37564267ca6c5cdfc27da807b343c133cf77651
souvik mndal <souvkmndal@gmail.com>
2024-11-02 20:20:07 +0530

    search element using binary search

---
+++ 03.Searching & Sorting/01.search using binary search.cpp
@@ -0,0 +1,12 @@
+//WAP to search a target element using binary search
+#include<bits/stdc++.h>
+using namespace std;
+int BS( int arr[] , int n , int x ){
+    int s=0 , e=n-1;
+    while( s<=e ){
+        
+    }
+}
+int main(){
+
+}
