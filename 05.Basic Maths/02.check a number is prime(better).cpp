11774fa24602acd940ca4e02bac11d303702ad0e
souvik mndal <souvkmndal@gmail.com>
2024-12-22 18:52:02 +0530

    finding prime number better approach

---
+++ 05.Basic Maths/02.check a number is prime(better).cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime( int n ){
+    if( n<=1 ){
+        return false;
+    }
+    for( int i=2 ; i<=sqrt(n) ; i++ ){
+        if( n % i==0 ){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    int n ;
+    cout<<"Enter the number : ";
+    cin>>n;
+    bool ans=isprime(n);
+    if( ans ){
+        cout<<"It's a prime number ";
+    }
+    else{
+        cout<<"Its isn't a prime number ";
+    }
+}
