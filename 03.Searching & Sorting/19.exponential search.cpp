98369dd0b66f638418e952d5f9c494bbfb6492fa
souvik mndal <souvkmndal@gmail.com>
2025-01-17 02:40:42 +0530

    Exponential search

---
+++ 03.Searching & Sorting/19.exponential search.cpp
@@ -0,0 +1,9 @@
+//its used when the search space is very big and the target is present near the starting of the array
+int i=1,j=n-1;
+if(arr[0]==target){
+    return 0;
+}
+while(i<=j && arr[i]<target){
+    i*=2;
+}
+int ans=bs(arr,i/2,min(i,n-1,target));
