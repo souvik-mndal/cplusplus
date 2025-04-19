4b02efd29e56cf84ce7af84f8032b0abeaf32c35
souvik mndal <souvkmndal@gmail.com>
2025-04-20 01:25:22 +0530

    add

--- 01.Basics/02.take input.cpp
+++
@@ -1,9 +0,0 @@
-//WAP to take input an print it
-#include<iostream>
-using namespace std;
-int main(){
-    int input;
-    cout<<"enter the number : ";
-    cin>>input;
-    cout<<"the input value is : "<<input;
-}
--- 01.Basics/07.print a name 11 times.cpp
+++
@@ -1,8 +0,0 @@
-//WAP to print a name 11 times
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    for( int i=0 ; i<11 ; i++ ){
-        cout<<"SM"<<endl;
-    }
-}
