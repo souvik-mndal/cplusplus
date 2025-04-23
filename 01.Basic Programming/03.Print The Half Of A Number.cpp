726afb5485803a9ec4f0fdccbb039fa438cd01ba
souvik mndal <souvkmndal@gmail.com>
2025-04-23 12:12:36 +0530

    Take User Input From User And Print The half of the number

---
+++ 01.Basic Programming/03.Print The Half Of A Number.cpp
@@ -0,0 +1,9 @@
+//WAP to take user input and print the half of that number
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter the number : ";
+    cin>>number;
+    cout<<"The number of the number you entered is : "<<number/2;
+}
