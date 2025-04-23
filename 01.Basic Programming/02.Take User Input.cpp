7738600d01ff241bcf540335179d64c23e394b2c
souvik mndal <souvkmndal@gmail.com>
2025-04-23 12:08:54 +0530

    Take User Input From The User

---
+++ 01.Basic Programming/02.Take User Input.cpp
@@ -0,0 +1,9 @@
+//WAP to take user input
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter the number : ";
+    cin>>number;
+    cout<<"The number you entered is : "<<number;
+}
