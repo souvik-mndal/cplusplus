f8eadae6ee054a9f12370b0bce6d5aa2db13e04f
souvik mndal <souvkmndal@gmail.com>
2025-04-23 12:15:18 +0530

    print the sum of two numbers

---
+++ 01.Basic Programming/04.sum of two numbers.cpp
@@ -0,0 +1,9 @@
+//WAP to take user input and print the sum of two numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int number1,number2;
+    cout<<"Enter the numbers : ";
+    cin>>number1>>number2;
+    cout<<"The sum is : "<<number1+number2;
+}
