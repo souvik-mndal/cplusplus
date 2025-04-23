be2db03963e8d22a13fe2a7db5eb1047ee35729c
souvik mndal <souvkmndal@gmail.com>
2025-04-23 12:22:02 +0530

    to find the area of a rectangle

---
+++ 01.Basic Programming/06.area of a rectangle.cpp
@@ -0,0 +1,9 @@
+//WAP to find the area of a rectangle
+#include<iostream>
+using namespace std;
+int main(){
+    int length,breadth;
+    cout<<"Enter the length and the breadth : ";
+    cin>>length>>breadth;
+    cout<<"the area is : "<<length*breadth;
+}
