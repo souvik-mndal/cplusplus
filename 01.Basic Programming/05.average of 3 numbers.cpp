c6a3aea0ca399a7961009abd87899f232414f851
souvik mndal <souvkmndal@gmail.com>
2025-04-23 12:18:25 +0530

    print the average of 3 numbers

---
+++ 01.Basic Programming/05.average of 3 numbers.cpp
@@ -0,0 +1,9 @@
+//WAP to print the average of 3 numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int number1,number2,number3;
+    cout<<"Enter the numbers : ";
+    cin>>number1>>number2>>number3;
+    cout<<"The average is : "<<(number1+number2+number3)/3;
+}
