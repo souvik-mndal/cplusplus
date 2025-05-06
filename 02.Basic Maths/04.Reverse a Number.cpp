ae6d892446704db33e1530fb2682cd695358f63f
souvik mndal <souvkmndal@gmail.com>
2025-05-06 18:40:09 +0530

    o reverse a given number

---
+++ 02.Basic Maths/04.Reverse a Number.cpp
@@ -0,0 +1,22 @@
+// WAP to reverse a given number.
+// Input: 1234
+// Output: 4321
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int rev = 0;
+    while(num != 0) {
+        int digit = num % 10;
+        rev = rev * 10 + digit;
+        num /= 10;
+    }
+
+    cout << "Reversed number: " << rev << endl;
+    return 0;
+}
